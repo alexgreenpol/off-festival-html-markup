b0a913e00be008d69e93ab50d3c8a90c2352e0a2
sukhrob.sanhinov <sukhrob.sanhinov@codeit.pro>
2023-04-14 16:44:02 +0300

    Update project, add new pages

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 const toggleButton = document.querySelector('.toggle-menu');
 const header = document.querySelector('.header');
 toggleButton.addEventListener('click', function () {
-  header.classList.toggle('toggle');
+	header.classList.toggle('toggle');
 });
 
 document.addEventListener('scroll', () => {
-  const header = document.querySelector('.header');
+	const header = document.querySelector('.header');
 
-  if (window.scrollY > 200) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
+	if (window.scrollY > 200) {
+		header.classList.add('scrolled');
+	} else {
+		header.classList.remove('scrolled');
+	}
 });
+
+/* ACCORDION */
+
+const acc = document.querySelectorAll(".accordion__btn");
+
+for (let i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+
+		const panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+}
